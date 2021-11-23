034ce1b288bf5c6c1a596c43e351c217745874d3
lefterav <eleftherios.avramidis@dfki.de>
2021-11-23 17:55:02 +0100

    Models conforming to Python scripting conventions. Added Languagefield in model where appropriate.

--- backend/models.py
+++ backend/models.py
@@ -5,32 +5,39 @@ from languages.fields import LanguageField
 ## Bucket models
 class BucketCategory(models.Model):
     name = models.CharField(max_length=25, primary_key=True)
-    source_language = models.CharField(max_length=15)
+    source_language = LanguageField()
+
 
 class Bucket(models.Model):
     bucket_category = models.ForeignKey(BucketCategory, on_delete=models.CASCADE)
     description = models.CharField(max_length=100)
 
+
 class BucketItem(models.Model):
     bucket = models.ForeignKey(Bucket, on_delete=models.CASCADE)
     token = models.CharField(max_length=30)
 
+
 ## Language pairs & Test Sets
 class Langpair(models.Model):
-    source_language = models.CharField(max_length=15)
-    target_language = models.CharField(max_length=15)
+    source_language = LanguageField()
+    target_language = LanguageField()
+
 
 class Testset(models.Model):
     description = models.CharField(max_length=250)
+
 
 ## from Categories to Rules
 class Category(models.Model):
     name = models.CharField(max_length=30, primary_key=True)
     langpair = models.ForeignKey(Langpair, on_delete=models.CASCADE)
 
+
 class Phenomenon(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     name = models.CharField(max_length=30)
+
 
 class TestItem(models.Model):
     testset = models.ForeignKey(Testset, on_delete=models.CASCADE)
@@ -38,7 +45,7 @@ class TestItem(models.Model):
     generation_time = models.DateField()
     source_sentence = models.CharField(max_length=1000)
 
-class Rule(models.Model):
+Rule(models.Model):
     item = models.ForeignKey(TestItem, on_delete=models.CASCADE)
     rule_string = models.CharField(max_length=200)
     prefix = models.BooleanField()
