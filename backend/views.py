1bc3f564debd193a34690c6d2720c1b3b6661fda
niemi <nieminentommi@hotmail.com>
2021-11-23 18:54:53 +0200

    rest api foundations

--- backend/views.py
+++ backend/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from backend.models import TestSet
+from rest_framework import viewsets
+from rest_framework import permissions
+from backend.serializers import TestSetSerializer
 
-# Create your views here.
+
+class TestSetViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = TestSet.objects.all()
+    serializer_class = TestSetSerializer
+    permission_classes = []#[permissions.IsAuthenticated]
+
