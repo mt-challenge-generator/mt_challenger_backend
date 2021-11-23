cb8b0e52d0555d3137857c7e17abada4d59cbb7a
lefterav <eleftherios.avramidis@dfki.de>
2021-11-24 00:35:08 +0100

    Typos and requirements

--- backend/models.py
+++ backend/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from languages.fields import LanguageField
 # Create your models here.
 
+
 ## Bucket models
 class BucketCategory(models.Model):
     name = models.CharField(max_length=25, primary_key=True)
@@ -44,6 +45,7 @@ class TestItem(models.Model):
     phenomenon = models.ForeignKey(Phenomenon, on_delete=models.CASCADE)
     generation_time = models.DateField()
     source_sentence = models.CharField(max_length=1000)
+
 
 class Rule(models.Model):
     item = models.ForeignKey(TestItem, on_delete=models.CASCADE)
