0bbd8c50925a6ada978f4578932b6fe777265b68
lefterav <eleftherios.avramidis@dfki.de>
2021-11-23 17:50:18 +0100

    First commit, no code

--- backend/models.py
+++ backend/models.py
@@ -1,18 +1,18 @@
 from django.db import models
-
+from languages.fields import LanguageField
 # Create your models here.
 
 ## Bucket models
-class Bucket_category(models.Model):
+class BucketCategory(models.Model):
     name = models.CharField(max_length=25, primary_key=True)
     source_language = models.CharField(max_length=15)
 
 class Bucket(models.Model):
-    bucket_categoryid = models.ForeignKey(Bucket_Category, on delete=models.CASCADE)
+    bucket_category = models.ForeignKey(BucketCategory, on_delete=models.CASCADE)
     description = models.CharField(max_length=100)
 
-class Bucket_item(models.Model):
-    bucket_id = models.ForeignKey(Bucket, on delete=models.CASCADE)
+class BucketItem(models.Model):
+    bucket = models.ForeignKey(Bucket, on_delete=models.CASCADE)
     token = models.CharField(max_length=30)
 
 ## Language pairs & Test Sets
@@ -25,20 +25,20 @@ class Testset(models.Model):
 
 ## from Categories to Rules
 class Category(models.Model):
-    langpair_id = models.ForeignKey(Langpair, on delete=models.CASCADE)
     name = models.CharField(max_length=30, primary_key=True)
+    langpair = models.ForeignKey(Langpair, on_delete=models.CASCADE)
 
 class Phenomenon(models.Model):
-    category_id = models.ForeignKey(Category, on delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     name = models.CharField(max_length=30)
 
-class Test_item(models.Model):
-    testset_id = models.ForeignKey(Testset, on delete=models.CASCADE)
-    phenomenon_id = models.ForeignKey(Phenomenon, on delete=models.CASCADE)
+class TestItem(models.Model):
+    testset = models.ForeignKey(Testset, on_delete=models.CASCADE)
+    phenomenon = models.ForeignKey(Phenomenon, on_delete=models.CASCADE)
     generation_time = models.DateField()
     source_sentence = models.CharField(max_length=1000)
 
 class Rule(models.Model):
-    item_id = models.ForeignKey(Test_item, on delete=models.CASCADE)
+    item = models.ForeignKey(TestItem, on_delete=models.CASCADE)
     rule_string = models.CharField(max_length=200)
     prefix = models.BooleanField()
